3c6d47c7aa17f79f885dc38a2a742a082cf2e557
Ihor Kvasha <kvashaihor@gmail.com>
2025-05-04 01:29:36 -0700

    commit 1.0

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,21 @@
-// export const Person = ({ person }) => ();
+export function Person({ person }) {
+  let partnerInfo;
+
+  if (!person.isMarried) {
+    partnerInfo = 'I am not married';
+  } else if (person.sex === 'm') {
+    partnerInfo = `${person.partnerName} is my wife`;
+  } else {
+    partnerInfo = `${person.partnerName} is my husband`;
+  }
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">My name is {person.name}</h2>
+      {person.age !== undefined && (
+        <p className="Person__age">I am {person.age}</p>
+      )}
+      <p className="Person__partner">{partnerInfo}</p>
+    </section>
+  );
+}

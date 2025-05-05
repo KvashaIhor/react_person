8ac8a370779786aac5951ec89030ef8b3c6f2f77
Ihor Kvasha <kvashaihor@gmail.com>
2025-05-05 14:34:40 -0700

    commit 1.4

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,20 +1,15 @@
 export function Person({ person }) {
-  let partnerInfo;
+  const { name, age, isMarried, sex, partnerName } = person;
 
-  if (!person.isMarried) {
-    partnerInfo = 'I am not married';
-  } else if (person.sex === 'm') {
-    partnerInfo = `${person.partnerName} is my wife`;
-  } else {
-    partnerInfo = `${person.partnerName} is my husband`;
-  }
+  const partnerRole = sex === 'm' ? 'wife' : 'husband';
+  const partnerInfo = !isMarried
+    ? 'I am not married'
+    : `${partnerName} is my ${partnerRole}`;
 
   return (
     <section className="Person">
-      <h2 className="Person__name">My name is {person.name}</h2>
-      {person.age !== undefined && (
-        <p className="Person__age">I am {person.age}</p>
-      )}
+      <h2 className="Person__name">My name is {name}</h2>
+      {age && <p className="Person__age">I am {age}</p>}
       <p className="Person__partner">{partnerInfo}</p>
     </section>
   );
